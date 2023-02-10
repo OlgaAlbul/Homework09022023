c9e73282cf6799f56d5b7ebb839d232bbb97abdd
AlbulOlya <albul.olya@gmail.com>
2023-02-10 22:29:56 +0500

    Task 3 solved

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[]collection)
+{
+    int length=collection.Length;
+    for (int index = 0; index < length; index++)
+    {
+        collection[index]=new Random().Next(1,100);
+    }
+}
+int[] numbers = new int[10];
+FillArray(numbers);
+int[] numbersCopy=new int[numbers.Length];
+int j=0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i]+" ");
+    numbersCopy[j]=numbers[i];
+    j++;
+}
+Console.WriteLine();
+Console.WriteLine("Копия массива:");
+for (int h = 0; h < numbersCopy.Length; h++)
+{
+    Console.Write(numbers[h]+" ");
+}
+
+
+
+
