6ff1e93cfe27daa9007624f76cce8b66af1796e6
AlbulOlya <albul.olya@gmail.com>
2023-02-10 18:54:00 +0500

    Task 1 solved

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите коэффициент K1");
+double numbK1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент B1");
+double numbB1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент K2");
+double numbK2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите коэффициент B2");
+double numbB2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine($" Первая прямая выражается уравнением: Y={numbK1}*x+{numbB1}");
+Console.WriteLine($" Вторая прямая выражается уравнением: Y={numbK2}*x+{numbB2}");
+double x=0;
+double y=0;
+if (numbK1==numbK2)
+{
+    Console.WriteLine("Прямые не имеют точек пересечения так как параллельны друг другу");
+}
+else
+{
+    x=(numbB2-numbB1)/(numbK1-numbK2);
+    y=numbK1*x+numbB1;
+    Console.WriteLine($"Прямые пересекаются в точке с координатами: X= {x} Y = {y}");
+}
